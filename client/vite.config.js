53a0afcf5b9f68fa71e7db69445778ed79fe2fac
Marko Mijakovski <m.mijakovski07@gmail.com>
2025-07-25 19:46:50 +0200

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   root: './client', 
   plugins: [react()],
-  base: '/web_One/',
+  base: '/',
   build: {
     outDir: './dist', // output dist folder relative to root
   },
