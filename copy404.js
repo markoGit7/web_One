3a8c40e1752a698b3c034bb73d8a5d56af3ac53d
Marko Mijakovski <m.mijakovski07@gmail.com>
2025-07-26 16:55:21 +0200

    .

--- copy404.js
+++ copy404.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Simulate __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const src = path.resolve(__dirname, 'client', 'dist', 'index.html');
 const dest = path.resolve(__dirname, 'client', 'dist', '404.html');
